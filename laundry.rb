c0efb21051839aea0025bdc1298d0ff5f6133327
techhirestudent <live_work_achieve@outlook.com>
2017-01-11 15:46:23 -0500

    first commit

---
+++ laundry.rb
@@ -0,0 +1,21 @@
+def wear_clothes(item)
+  sample(item)
+  sleep(1)
+end
+
+def wash_clothes(temperature)
+  play(temperature)
+  sleep(1)
+end
+def dry_clothes(time)
+  sample(:drum_tom_hi_hard)
+  sleep(time)
+end
+2.times do
+  wear_clothes(:drum_tom_hi_hard)
+  wash_clothes(60)
+  dry_clothes(2)
+end
+wear_clothes(:bass_hard_c)
+wash_clothes(50)
+dry_clothes(2)
